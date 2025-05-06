4e0bcd5ec987a841cfbc9bca6908bf73b0b2fd5f
ekrsw <eisuke.koresawa@gmail.com>
2025-05-06 19:50:34 +0900

    M

--- user-service/app/crud_test.py
+++ user-service/app/crud_test.py
@@ -3,24 +3,24 @@ import asyncio
 from sqlalchemy import inspect
 
 from app.crud.user import user_crud
-from app.crud.group import group_crud
 from app.schemas.user import UserCreate
-from app.schemas.group import GroupCreate
 from app.db.init import Database
 from app.db.session import async_engine, AsyncSessionLocal
 
 
 async def main():
+    
     # データベース初期化処理
     db = Database()
     await db.init()
 
-    # 登録するユーザー情報
     user_in = UserCreate(
-        group_name="test_group"
+        username="Koresawa Eisuke",
+        email="eisuke_koresawa@example.com",
     )
+    
     async with AsyncSessionLocal() as session:
-        await group_crud.create(session, user_in)
+        await user_crud.create(session, user_in)
         await session.commit()
         
 if __name__ == "__main__":

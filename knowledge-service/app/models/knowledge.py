68f3bdafffc81f2bf56b5e5bd1fb89f2d02f92e5
ekrsw <eisuke.koresawa@gmail.com>
2025-05-10 22:22:24 +0900

    M

--- knowledge-service/app/models/knowledge.py
+++ knowledge-service/app/models/knowledge.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Boolean, String
+from datetime import date
+
+from sqlalchemy import Boolean, Date, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Optional
 
@@ -8,5 +10,16 @@ from app.db.base import Base
 class Knowledge(Base):
     __tablename__ = "knowledge"
 
-    username: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
-    email: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    title: Mapped[str] = mapped_column(String, nullable=False, unique=False, index=True)
+    info_category: Mapped[int] = mapped_column(Integer, unique=False, nullable=False, default=0)
+    key_words: Mapped[Optional[str]] = mapped_column(String, unique=False, nullable=True, index=True)
+    importance: Mapped[Boolean] = mapped_column(Boolean, nullable=False, default=False)
+    start_published: Mapped[Optional[date]] = mapped_column(Date, unique=False, nullable=True)
+    end_published: Mapped[Optional[date]] = mapped_column(Date, unique=False, nullable=True)
+    target: Mapped[int] = mapped_column(Integer, unique=False, nullable=False, default=0)
+    question: Mapped[str] = mapped_column(String, unique=False, nullable=True, index=True)
+    answer: Mapped[str] = mapped_column(String, unique=False, nullable=True, index=True)
+    add_comments: Mapped[Optional[str]] = mapped_column(String, unique=False, nullable=True, index=True)
+    remarks: Mapped[Optional[str]] = mapped_column(String, unique=False, nullable=True, index=True)
+
+    status: Mapped[int] = mapped_column(Integer, unique=False, nullable=False, default=0)
